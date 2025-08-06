9050e42520157ff69bf5272004c3003c3858beb1
maheshreddysama <samamahesh0079@gmail.com>
2025-08-05 22:52:17 -0400

    updated locustfile.py

--- penguin_classification/locustfile.py
+++ penguin_classification/locustfile.py
@@ -1,32 +1,22 @@
 from locust import HttpUser, task, between
 
 class MultiAppUser(HttpUser):
-    wait_time = between(1, 5)
-
-    @task
-    def predict_local(self):
-        payload = {
-            "species": "Adelie",
-            "island": "Torgersen",
-            "bill_length_mm": 39.1,
-            "bill_depth_mm": 18.7,
-            "flipper_length_mm": 181.0,
-            "body_mass_g": 3750.0,
-            "sex": "male",
-        }
-        #self.client.post("http://localhost:8000/predict", json=payload)
-        self.client.post("http://localhost:8080/predict", json=payload)
-
+    # Set the host to your Cloud Run service URL
+    host = "https://penguin-api-408534553654.us-central1.run.app"
+    # For local testing
+    host = "http://localhost:8080"
+    wait_time = between(1, 5) # Wait time between tasks for each user
 
     @task
     def predict_cloud(self):
+        # This payload matches the PenguinFeatures model in your main.py
         payload = {
-            "species": "Adelie",
-            "island": "Torgersen",
             "bill_length_mm": 39.1,
             "bill_depth_mm": 18.7,
             "flipper_length_mm": 181.0,
             "body_mass_g": 3750.0,
             "sex": "male",
+            "island": "Torgersen",
         }
-        self.client.post("https://penguin-api-408534553654.us-central1.run.app/predict", json=payload)
+        # Use a relative path now that the host is defined at the class level
+        self.client.post("/predict", json=payload)
